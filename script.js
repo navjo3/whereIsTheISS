85874e55bbc36fc784cecc19f32e6060d42f76c3
navjo3 <96572213+navjo3@users.noreply.github.com>
2024-08-22 21:02:45 +0400

    Added markers and customisations

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
-const map = L.map('issMap').setView([0, 0], 0);
-const marker = L.marker([0, 0]).addTo(map);
+const map = L.map('issMap').setView([0, 0], 1);
 
-        const iss_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+const iss_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+
+//icon customization
+const myIcon = L.icon({
+    iconUrl: 'iss.png',
+    iconSize: [50, 50],
+    iconAnchor: [0, 0],
+});
+
+//map pointer
+const marker = L.marker([0, 0],{icon: myIcon}).addTo(map);
 
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
-        
+//tiles loading
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
 
 
-        //function to get json
-        async function getISS() {
-            const response = await fetch(iss_url);
-            const data = await response.json();
-            const {latitude, longitude} = data;
-            console.log(latitude);
-            console.log(longitude);
 
-            marker.setLatLng([latitude,longitude]);
+//function to fetch iss location json
+async function getISS() {
+    const response = await fetch(iss_url);
+    const data = await response.json();
+    const {latitude, longitude} = data;
+    console.log(latitude);
+    console.log(longitude);
 
-            document.getElementById('lat').textContent = latitude;
-            document.getElementById('lon').textContent = longitude;
 
-        }
+    marker.setLatLng([latitude,longitude]);
 
-        //call function
-        getISS();
+    document.getElementById('lat').textContent = latitude;
+    document.getElementById('lon').textContent = longitude;
+
+    marker.bindTooltip('<div class="custom-tooltip">Latitude: ${latitude}, <br> Longitude: ${longitude}').openTooltip();
+}
+
+//call function
+getISS();
